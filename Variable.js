e4ec473b7d6eb5279c608274eaec641730c5a29d
simrat21122401 <simransaini240102@gmail.com>
2024-02-12 14:31:42 +0530

    second change

---
+++ Variable.js
@@ -0,0 +1,20 @@
+//variables- const, var, let memory ke andr assign krna
+
+//const - cannot change
+
+//console.log([  ]) - srye variables ko ek sath lekr chlta h in the form of table
+
+// prefer not to use (var)because of issue in the block scope and functional scope.
+
+var fullName = "panda";
+var fullName = "mr.panda"
+console.log(fullName);
+
+let name1 = "you"
+name2 = "me"
+console.log(name1);
+
+const panda = "bgarh"
+const panda1 = "gurugram"
+console.log("panda")
+
